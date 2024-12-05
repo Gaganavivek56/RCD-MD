fd7f9c8991b2ef122ec79dacecb7f114aeb85f8b
Gaganavivek56 <gaganavivekk@gmail.com>
2024-12-05 08:10:46 +0530

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -18,7 +18,7 @@ const config = {
   
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
-  MODE: process.env.MODE || "private",
+  MODE: process.env.MODE || "public",
   PREFIX: process.env.PREFIX || "!",
   OWNER_NAME: process.env.OWNER_NAME || "Gagan_MD",
   SUDO_NUMBER: process.env.OWNER_NUMBER || "+94759722028",
